008cedff53a57e26f46872e8359daa982871e6ad
oleksandra <sasha.shergina@gmail.com>
2026-05-03 19:50:43 +0300

    added k8s logic to facade, some code refactoring

--- facade/app.py
+++ facade/app.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 import httpx
 import asyncio
 import time
+import random
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 import hazelcast
+from kubernetes import client, config
 
 class ClientMessage(BaseModel):
     user_Id: int
@@ -17,6 +19,7 @@ class ServiceState:
     count_time: float = 0
 
 hz_mq = None
+v1 = None
 state = ServiceState()
 
 @asynccontextmanager
@@ -28,6 +31,10 @@ async def lifespan(app: FastAPI):
         cluster_members=["hazelcast-node:5701"],
         cluster_name="dev"
     )
+
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+
     global hz_mq
     hz_mq = client.get_queue("message-queue")
 
@@ -48,49 +55,69 @@ async def measure_request(func, *args, **kwargs):
         end_time = time.perf_counter()
         return response, end_time - start_time
 
+def get_service_ips(service_name, namespace="default"):
+    endpoints = v1.read_namespaced_endpoints(service_name, namespace)
+    instances = []
+    for subset in endpoints.subsets:
+        for address in subset.addresses:
+            for port in subset.ports:
+                instances.append({
+                    "ip": address.ip,
+                    "port": port.port,
+                })
+    return instances
+
 @app.post("/facade_service")
 async def post_facade(msg: ClientMessage):
 
     timestamp = int(time.time())
 
     hz_mq.offer({"transaction_ID": timestamp,
-                                         "user_Id": msg.user_Id,
-                                         "amount": msg.amount})
+                 "user_Id": msg.user_Id,
+                 "amount": msg.amount})
 
 
-    # TODO remove static address, add k8s logic
-    log_addr = f"http://logging-service/logging_service"
+    logging_addresses = get_service_ips("logging-service")
+    if not logging_addresses:
+        return {"error": "logging-service unavailible"}
+
+    log_addr = random.choice(logging_addresses)
     log_task = measure_request(state.client.post, log_addr, json={"transaction_ID": timestamp,
                                                             "user_Id": msg.user_Id,
                                                             "amount": msg.amount})
     _, log_t = await(log_task)
     state.log_time += log_t
     return {"transaction_ID": timestamp}
-    # return {"transaction_ID": "logging-service unavailible"}
 
 @app.get("/facade_service/user/{userId}")
 async def get_user_balance_transactions(userId: str):
 
     tasks = []
-    # TODO
-    log_addr = f"http://logging-service/logging_service"
-    log_task = measure_request(state.client.get, log_addr+"/user/"+userId)
-    tasks.append(log_task)
+    logging_addresses = get_service_ips("logging-service")
+
+    if logging_addresses:
+        log_addr = random.choice(logging_addresses)
+        log_task = measure_request(state.client.get, f"http://{log_addr["ip"]}:{log_addr["port"]}/user/{userId}")
+        tasks.append(log_task)
     
 
-    count_addr = f"http://counter-service/counter_service"
-    count_task = measure_request(state.client.get, count_addr+"/user/"+userId)
-    tasks.append(count_task)
+    counter_addresses = get_service_ips("counter-service")
+    if counter_addresses:
+        count_addr = random.choice(counter_addresses)
+        count_task = measure_request(state.client.get, f"http://{count_addr["ip"]}:{count_addr["port"]}/user/{userId}")
+        tasks.append(count_task)
 
     result = await asyncio.gather(*tasks)
     log_response, count_response = None, None
     i = 0
 
-    log_response = result[0][0].json() if not isinstance(result[0][0], Exception) else None
-    state.log_time += result[0][1]
-    i = 1
-    count_response = result[i][0].json()["balance"] if not isinstance(result[i][0], Exception) else None
-    state.count_time += result[i][1]
+    if logging_addresses:
+        log_response = result[0][0].json() if not isinstance(result[0][0], Exception) else None
+        state.log_time += result[0][1]
+        i = 1
+    if counter_addresses:
+        count_response = result[i][0].json()["balance"] if not isinstance(result[i][0], Exception) else None
+        state.count_time += result[i][1]
         
     return {"balance": count_response,
             "transactions": log_response}
@@ -98,14 +125,16 @@ async def get_user_balance_transactions(userId: str):
 
 @app.get("/facade_service/accounts")
 async def get_accounts():
-    # TODO
-    count_addr = f"http://counter-service/counter_service"
+    counter_addresses = get_service_ips("counter-service")
+    if counter_addresses:
+        count_addr = random.choice(counter_addresses)
 
-    count_task = measure_request(state.client.get, count_addr+"/accounts")
-    (count_response, count_t) = await count_task
-    state.count_time += count_t
-    if not isinstance(count_response, Exception):
-        return count_response.json()
+        count_task = measure_request(state.client.get, count_addr+"/accounts")
+        (count_response, count_t) = await count_task
+        state.count_time += count_t
+        if not isinstance(count_response, Exception):
+            return count_response.json()
+    return {"error": "counter-service unavailable"}
 
 
 
